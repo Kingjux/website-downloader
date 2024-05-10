31c379d53a7e6d2ad288ec33c2ddb1d115f8baaa
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-05-10 18:53:06 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Website downloader - Take any website offline.' });
+  res.render('index', { title: 'Gifted Website Downloader - Get Source Code and All Files of Any Website.' });
 });
 
 module.exports = router;
